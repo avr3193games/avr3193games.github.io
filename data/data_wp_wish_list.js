46d142e8443e6421d588f9dcc0a2ec1034617a26
avr3193games <100206168+avr3193games@users.noreply.github.com>
2022-07-29 01:08:15 +0530

    New Project - Wish Tracker

---
+++ data/data_wp_wish_list.js
@@ -0,0 +1,281 @@
+document.addEventListener('alpine:init', () => {
+  Alpine.store('wp_wish_list', {
+    list: [{
+      name: 'Akuoumaru',
+      rarity: 4,
+      id: 'akuoumaru'
+        }, {
+      name: 'Alley Hunter',
+      rarity: 4,
+      id: 'alley_hunter'
+        }, {
+      name: 'Amos\' Bow',
+      rarity: 5,
+      id: 'amos_bow'
+        }, {
+      name: 'Aqua Simulacra',
+      rarity: 5,
+      id: 'aqua_simulacra'
+        }, {
+      name: 'Aquila Favonia',
+      rarity: 5,
+      id: 'aquila_favonia'
+        }, {
+      name: 'Black Tassel',
+      rarity: 3,
+      id: 'black_tassel'
+        }, {
+      name: 'Bloodtainted Greatsword',
+      rarity: 3,
+      id: 'bloodtainted_greatsword'
+        }, {
+      name: 'Calamity Queller',
+      rarity: 5,
+      id: 'calamity_queller'
+        }, {
+      name: 'Cool Steel',
+      rarity: 3,
+      id: 'cool_steel'
+        }, {
+      name: 'Debate Club',
+      rarity: 3,
+      id: 'debate_club'
+        }, {
+      name: 'Dragon\'s Bane',
+      rarity: 4,
+      id: 'dragons_bane'
+        }, {
+      name: 'Elegy for the End',
+      rarity: 5,
+      id: 'elegy_for_the_end'
+        }, {
+      name: 'Emerald Orb',
+      rarity: 3,
+      id: 'emerald_orb'
+        }, {
+      name: 'Engulfing Lightning',
+      rarity: 5,
+      id: 'engulfing_lightning'
+        }, {
+      name: 'Everlasting Moonglow',
+      rarity: 5,
+      id: 'everlasting_moonglow'
+        }, {
+      name: 'Eye of Perception',
+      rarity: 4,
+      id: 'eye_of_perception'
+        }, {
+      name: 'Favonius Codex',
+      rarity: 4,
+      id: 'favonius_codex'
+        }, {
+      name: 'Favonius Greatsword',
+      rarity: 4,
+      id: 'favonius_greatsword'
+        }, {
+      name: 'Favonius Lance',
+      rarity: 4,
+      id: 'favonius_lance'
+        }, {
+      name: 'Favonius Sword',
+      rarity: 4,
+      id: 'favonius_sword'
+        }, {
+      name: 'Favonius Warbow',
+      rarity: 4,
+      id: 'favonius_warbow'
+        }, {
+      name: 'Ferrous Shadow',
+      rarity: 3,
+      id: 'ferrous_shadow'
+        }, {
+      name: 'Freedom-Sworn',
+      rarity: 5,
+      id: 'freedom-sworn'
+        }, {
+      name: 'Haran Geppaku Futsu',
+      rarity: 5,
+      id: 'haran_geppaku_futsu'
+        }, {
+      name: 'Harbinger of Dawn',
+      rarity: 3,
+      id: 'harbinger_of_dawn'
+        }, {
+      name: 'Kagura\'s Verity',
+      rarity: 5,
+      id: 'kaguras_verity'
+        }, {
+      name: 'Lion\'s Roar',
+      rarity: 4,
+      id: 'lions_roar'
+        }, {
+      name: 'Lithic Blade',
+      rarity: 4,
+      id: 'lithic_blade'
+        }, {
+      name: 'Lithic Spear',
+      rarity: 4,
+      id: 'lithic_spear'
+        }, {
+      name: 'Lost Prayer to the Sacred Winds',
+      rarity: 5,
+      id: 'lost_prayer_to_the_sacred_winds'
+        }, {
+      name: 'Magic Guide',
+      rarity: 3,
+      id: 'magic_guide'
+        }, {
+      name: 'Memory of Dust',
+      rarity: 5,
+      id: 'memory_of_dust'
+        }, {
+      name: 'Mistsplitter Reforged',
+      rarity: 5,
+      id: 'mistsplitter_reforged'
+        }, {
+      name: 'Mitternachts Waltz',
+      rarity: 4,
+      id: 'mitternachts_waltz'
+        }, {
+      name: 'Mouun\'s Moon',
+      rarity: 4,
+      id: 'mouuns_moon'
+        }, {
+      name: 'Polar Star',
+      rarity: 5,
+      id: 'polar_star'
+        }, {
+      name: 'Primordial Jade Cutter',
+      rarity: 5,
+      id: 'primordial_jade_cutter'
+        }, {
+      name: 'Primordial Jade Winged-Spear',
+      rarity: 5,
+      id: 'primordial_jade_winged-spear'
+        }, {
+      name: 'Rainslasher',
+      rarity: 4,
+      id: 'rainslasher'
+        }, {
+      name: 'Raven Bow',
+      rarity: 3,
+      id: 'raven_bow'
+        }, {
+      name: 'Redhorn Stonethresher',
+      rarity: 5,
+      id: 'redhorn_stonethresher'
+        }, {
+      name: '	Rust',
+      rarity: 4,
+      id: 'rust'
+        }, {
+      name: 'Sacrificial Bow',
+      rarity: 4,
+      id: 'sacrificial_bow'
+        }, {
+      name: 'Sacrificial Fragments',
+      rarity: 4,
+      id: 'sacrificial_fragments'
+        }, {
+      name: 'Sacrificial Greatsword',
+      rarity: 4,
+      id: 'sacrificial_greatsword'
+        }, {
+      name: 'Sacrificial Sword',
+      rarity: 4,
+      id: 'sacrificial_sword'
+        }, {
+      name: 'Sharpshooter\'s Oath',
+      rarity: 3,
+      id: 'sharpshooters_oath'
+        }, {
+      name: 'Skyrider Sword',
+      rarity: 3,
+      id: 'skyrider_sword'
+        }, {
+      name: 'Skyward Atlas',
+      rarity: 5,
+      id: 'skyward_atlas'
+        }, {
+      name: 'Skyward Blade',
+      rarity: 5,
+      id: 'skyward_blade'
+        }, {
+      name: 'Skyward Harp',
+      rarity: 5,
+      id: 'skyward_harp'
+        }, {
+      name: 'Skyward Pride',
+      rarity: 5,
+      id: 'skyward_pride'
+        }, {
+      name: 'Skyward Spine',
+      rarity: 5,
+      id: 'skyward_spine'
+        }, {
+      name: 'Slingshot',
+      rarity: 3,
+      id: 'slingshot'
+        }, {
+      name: 'Song of Broken Pines',
+      rarity: 5,
+      id: 'song_of_broken_pines'
+        }, {
+      name: 'Staff of Homa',
+      rarity: 5,
+      id: 'staff_of_homa'
+        }, {
+      name: 'Summit Shaper',
+      rarity: 5,
+      id: 'summit_shaper'
+        }, {
+      name: 'The Alley Flash',
+      rarity: 4,
+      id: 'the_alley_flash'
+        }, {
+      name: 'The Bell',
+      rarity: 4,
+      id: 'the_bell'
+        }, {
+      name: 'The Flute',
+      rarity: 4,
+      id: 'the_flute'
+        }, {
+      name: 'The Stringless',
+      rarity: 4,
+      id: 'the_stringless'
+        }, {
+      name: 'The Unforged',
+      rarity: 5,
+      id: 'the_unforged'
+        }, {
+      name: 'The Widsith',
+      rarity: 4,
+      id: 'the_widsith'
+        }, {
+      name: 'Thrilling Tales of Dragon Slayers',
+      rarity: 3,
+      id: 'thrilling_tales_of_dragon_slayers'
+        }, {
+      name: 'Thundering Pulse',
+      rarity: 5,
+      id: 'thundering_pulse'
+        }, {
+      name: 'Vortex Vanquisher',
+      rarity: 5,
+      id: 'vortex_vanquisher'
+        }, {
+      name: 'Wavebreaker\'s Fin',
+      rarity: 4,
+      id: 'wavebreakers_fin'
+        }, {
+      name: 'Wine and Song',
+      rarity: 4,
+      id: 'wine_and_song'
+        }, {
+      name: 'Wolf\'s Gravestone',
+      rarity: 5,
+      id: 'wolfs_gravestone'
+        }]
+  })
+})
